2eea2bd240c8e57701d86bccddd7f91b27b6aeb4
shital <shitaljadhav@2194@gamil.com>
2023-01-18 12:35:56 -0800

    First commit

---
+++ src/com/velocity/Sum.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		    
+		    int a = 20;
+			int b = 30;
+			int sum = a+b;
+			System.out.println(sum);
+	}
+
+}
